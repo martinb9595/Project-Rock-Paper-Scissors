b88ad993d71a948467acbf6230f546ec65e3369d
Martin Boev <83338805+martinb9595@users.noreply.github.com>
2024-04-08 18:36:57 +0300

    Solve challenge RPC

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let player = 0;
+let computer = 0;
+
 function getComputerChoice() {
   const computerChoices = ["rock", "paper", "scissors"];
   asd = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -6,19 +9,48 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return "It's a tie!";
+    return console.log("It's a tie!");
   } else if (
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
-    return "You Win! " + playerSelection + " beats " + computerSelection;
+    return (
+      console.log(
+        "You Win! " + playerSelection + " beats " + computerSelection
+      ),
+      (player = player + 1)
+    );
   } else {
-    return "You Lose! " + computerSelection + " beats " + playerSelection;
+    return (
+      console.log(
+        "You Lose! " + computerSelection + " beats " + playerSelection
+      ),
+      (computer = computer + 1)
+    );
   }
 }
 
 const playerSelection = prompt().toLowerCase();
 const computerSelection = getComputerChoice();
+const computerSelection1 = getComputerChoice();
+const computerSelection2 = getComputerChoice();
+const computerSelection3 = getComputerChoice();
+const computerSelection4 = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+  playRound(computerSelection, playerSelection);
+  playRound(computerSelection1, playerSelection);
+  playRound(computerSelection2, playerSelection);
+  playRound(computerSelection3, playerSelection);
+  playRound(computerSelection4, playerSelection);
+}
+
+function totalScore() {
+  if (player > computer) {
+    return console.log("Player Wins");
+  } else return console.log("Computer Wins");
+}
+
+playGame();
+totalScore();
